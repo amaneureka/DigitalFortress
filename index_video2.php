1a0b15524817e38b68ac8d3dae9ef479450165e7
Aman Priyadarshi <aman.eureka@gmail.com>
2016-03-06 02:26:24 +0530

    Base Template

---
+++ index_video2.php
@@ -0,0 +1,249 @@
+<!DOCTYPE html>
+<!--[if IE 8]><html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]>
+<!--><html class="no-js" lang="en"><!--<![endif]-->
+
+<!-- Mirrored from www.creabox.es/themes/verde/theme/index_video2.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 05 Mar 2016 19:55:31 GMT -->
+<head>
+	<meta charset="utf-8" />
+    
+    <!-- Website Title & Description -->
+    <title>Verde - Minimal Coming Soon Template</title>
+    <meta name="description" content="Verde - Minimal Coming Soon Template">
+    
+    <!-- Set the viewport width to device width for mobile -->
+    <meta name="viewport" content="width=device-width" />
+    <meta name="HandheldFriendly" content="True" />
+    <meta name="MobileOptimized" content="320" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="img/favicon/apple-touch-icon-144x144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/favicon/apple-touch-icon-114x114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/favicon/apple-touch-icon-72x72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="img/favicon/apple-touch-icon-57x57-precomposed.png">
+    <link rel="shortcut icon" href="img/favicon/favicon.png" />
+    
+    <!-- Included CSS Files -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style3.css">
+   
+    <!-- Google Web Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Exo:100,200,300,400,600,700,800,900,200italic,300italic,400italic' rel='stylesheet' type='text/css'>
+    
+    <!-- DEMO Switcher
+  	================================================== -->
+    <link rel="stylesheet" href="styleswitcher/css/styleswitcher.css" /> 
+    
+</head>
+<body>
+	
+	<div id="ut_styleswitcher">
+		<div style="position: relative;">
+            <a href="#" class="toggle"></a>
+            <h1>Version Switcher</h1>
+            <p>
+     		<a class="button-switcher" href="index.php">Image version</a>
+     		<a class="button-switcher" href="index_video.php">Video version</a>
+     		<a class="button-switcher" href="index_video2.php">Video version Youtube</a>
+     		<a class="button-switcher" href="index_video3.php">Video version Vimeo <span>(with cookies message)</span></a>
+     		<a class="button-switcher" href="index_canvas.php">Abstract version</a>
+     		</p>
+            <p class="info">This Panel is only for demo purposes! </p>
+        </div>
+    </div>
+	
+	<!-- Loader -->
+	<div id="loading">
+    	<img src="img/loader.gif" alt="Website Loader"/>
+    </div>
+    
+    <!-- Image replacement in mobile and tablets devices -->
+	<div id="bgimg"></div>
+    
+    <!-- Slideshow Pattern-->
+    <div class="slideshow-pattern"></div>
+    
+    <div class="container">
+    <div class="row">
+    <div class="span8 offset2" id="home">
+    
+    	<div id="part1" class="fadeout_1">
+	        <a class="a-rounded" href="#" title=""><h1>V</h1></a>
+	        <h3>Verde is coming soon! It will bring a lot of new features. It will return in:</h3>
+	        <p class="timer">
+	          	<span class="days"></span><span class="daysText"></span><span class="separate">/</span> 
+	          	<span class="hours"></span><span class="hoursText"></span><span class="separate">/</span>
+	          	<span class="minutes"></span><span class="minutesText"></span><span class="separate">/</span>
+	          	<span class="seconds"></span><span class="secondsText"></span> 
+			</p>
+		</div>	
+		
+		<div id="part2" class="fadeout_2">
+			<div class="row">
+			<div class="span8 links">
+				<div class="row">
+				
+				<div id="part2_1" class="fadeout_2_1">
+					<div class="span2 offset2">
+						<a href="#myModal" id="meetus" data-toggle="modal">MEET US</a>
+					</div>
+				</div>
+				
+				<div id="part2_2" class="fadeout_2_2">		
+					<div class="span2">
+						<a id="link-map" href="#map">CONTACT US</a>
+					</div>
+				</div>
+				
+				</div>
+			</div>
+			</div> 
+		</div>
+			
+		<div id="part3" class="fadeout_3">
+			<div class="singup" >
+				<div class="subscribe-mailchimp">
+                    <p>Sign up now to our newsletter and you'll be one of the first to know the new site:</p>
+					<div id="mc_embed_signup">
+						<form id="mc-form">
+					        <input id="mc-email" type="email" placeholder="email" class="email">
+					        <button type="submit" class="btn"><span class="singup-image" ><img src="img/send.png" alt="send email" /></span><span class="singup-text">Send</span></button>
+					        <label for="mc-email"></label>
+					    </form>
+					</div>
+                </div>
+			</div>
+		</div>
+		
+	    </div>
+    </div>
+    </div>
+
+    	<!-- Modal -->
+		<div id="myModal" class="modal hide fade meet-us" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+			<div class="modal-header">
+				<a href="#" class="close" data-dismiss="modal" aria-hidden="true"><img class="close-1" src="img/team/close2.png" alt="Close" /><img class="close-2" src="img/team/close1.png" alt="Close" /></a>
+			</div>
+			<div class="modal-body">
+				<h2 id="myModalLabel" class="meet-us-title">MEET US</h2>
+				<h4>Lorem ipsum dolor sit amet consectetur adipiscing elit, lacus tellus commodo, eu sagittis mi dolor nec nisi. </h4>
+				<div class="team first">
+					<img src="img/team/team1.jpg" alt="member1" />
+					<div class="team-social">
+					<p class="team-member">John Doe &#183; CEO</p>
+					<ul class="team-social-icons">
+						<li><a href="#" title="Facebook"><span class='symbol'>facebook</span></a></li>
+						<li><a href="#" title="Twitter"><span class='symbol'>twitter</span></a></li>
+						<li><a href="#" title="Google Plus"><span class='symbol'>googleplus</span></a></li>
+						<li><a href="#" title="Linkedin"><span class='symbol'>linkedin</span></a></li>
+					</ul>
+					</div>
+				</div>
+				<div class="team">
+					<img src="img/team/team2.jpg" alt="member2" />
+					<div class="team-social">
+					<p class="team-member">John Doe &#183; Web designer</p>
+					<ul class="team-social-icons">
+						<li><a href="#" title="Facebook"><span class='symbol'>facebook</span></a></li>
+						<li><a href="#" title="Twitter"><span class='symbol'>twitter</span></a></li>
+						<li><a href="#" title="Google Plus"><span class='symbol'>googleplus</span></a></li>
+						<li><a href="#" title="Linkedin"><span class='symbol'>linkedin</span></a></li>
+					</ul>
+					</div>
+				</div>
+			</div>
+			<div class="modal-footer">
+				<ul id="twitter-feed" class="list-tweets"></ul>
+			</div>
+		</div>
+    
+    
+    
+    <div class="map-content">
+    
+    <div class="slideshow-pattern-map"></div>
+    
+    <div id="map"></div>
+	
+	<div class="container">
+	<div class="row">
+		<div class="span6 offset3 contact">	
+		
+		<div id="part4">
+            <h2>CONTACT US</h2>
+            <h4>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</h4>
+        </div>
+            <form action="#" id="contactform">
+                <p id="part5_1">
+                	<input type="text" name="name" class="input" placeholder="Name">
+				</p>
+				<p id="part5_2">
+					<input type="text" name="email" class="input" placeholder="Email">
+				</p>
+				<p id="part5_3">
+					<textarea name="message" cols="35" rows="5" class="textarea" placeholder="Message"></textarea>
+				</p>
+				<p id="part5_4">
+					<button class="button" type="submit" value="Send"><span class="singup-image"><img src="img/send.png" alt="send email" /></span><span class="singup-text">Send</span></button>
+				</p>
+			</form>
+			<div class="success-message-2"></div>
+            <div class="error-message-2"></div> 
+		</div>            	
+	</div> 
+	</div>       
+    
+    
+    <div class="footer">
+    <div id="part6">
+	<div class="container">
+		<div class="span12 back-to-top">
+			<a href="#home" id="home-top"><img class="back-to-top-1" src="img/back-to-top.png" alt="Go back to top" /><img class="back-to-top-2" src="img/back-to-top-hover.png" alt="Go back to top" /></a>
+		</div>
+		<div class="row">
+			<div class="span12">
+			<p class="footer-social">				
+				<a href="#" title="Facebook" data-gal="tooltip" data-placement="top" data-original-title="Facebook"><span class='symbol'>&#xe227;</span></a>
+				<a href="#" title="Twitter" data-gal="tooltip" data-placement="top" data-original-title="Twitter"><span class='symbol'>&#xe286;</span></a>
+				<a href="#" title="Pinterest" data-gal="tooltip" data-placement="top" data-original-title="Pinterest"><span class='symbol'>&#xe264;</span></a>
+				<a href="#" title="Skype" data-gal="tooltip" data-placement="top" data-original-title="Skype"><span class='symbol'>&#xe274;</span></a>
+				<a href="#" title="Flickr" data-gal="tooltip" data-placement="top" data-original-title="Flickr"><span class='symbol'>&#xe229;</span></a>
+				<a href="#" title="Google Plus" data-gal="tooltip" data-placement="top" data-original-title="Google Plus"><span class='symbol'>&#xe239;</span></a>
+				<a href="#" title="Picassa" data-gal="tooltip" data-placement="top" data-original-title="Picassa"><span class='symbol'>&#xe263;</span></a>
+				<a href="#" title="Linkedin" data-gal="tooltip" data-placement="top" data-original-title="Linkedin"><span class='symbol'>&#xe252;</span></a>
+				<a href="#" title="Youtube" data-gal="tooltip" data-placement="top" data-original-title="Youtube"><span class='symbol'>&#xe299;</span></a>
+				<a href="#" title="Instagram" data-gal="tooltip" data-placement="top" data-original-title="Instagram"><span class='symbol'>&#xe300;</span></a>
+			</p>
+			</div>
+		</div>
+	</div>
+	</div>
+	</div>
+	
+    </div> <!-- /map-content -->
+ 	
+ 	<a id="bgndVideo"  data-property="{videoURL:'http://www.youtube.com/watch?v=qkfGP4UTpjE',containment:'body',autoPlay:true, mute:false, startAt:0,opacity:1,ratio:'4/3', addRaster:true}">My video</a>
+ 	 
+	<!-- Javascript -->
+    <script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/device.min.js"></script>
+	<script type="text/javascript" src="js/jquery.countdown.js"></script>
+	<script type="text/javascript" src="js/jquery.ajaxchimp.js"></script>
+	<script type="text/javascript" src="js/ajaxchimp.js"></script>
+	<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+    <script type="text/javascript" src="js/jquery.gmap.min.js"></script>
+    <script type="text/javascript" src="js/jquery.scrollTo-1.4.3.1-min.js"></script>
+    <script type="text/javascript" src="js/scripts.js"></script>
+    <script type="text/javascript" src="js/jquery.mb.YTPlayer.js"></script>
+    <script type="text/javascript" src="js/video_youtube.js"></script>
+  	
+  	<!-- DEMO Switcher
+  	================================================== -->
+  	<script src="styleswitcher/js/styleswitcher.js"></script>
+  	
+</body>
+
+<!-- Mirrored from www.creabox.es/themes/verde/theme/index_video2.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 05 Mar 2016 19:55:33 GMT -->
+</html>
