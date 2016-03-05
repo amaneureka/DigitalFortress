73dac9949220298c2ad978af0da5967f9d6edd4c
Aman Priyadarshi <aman.eureka@gmail.com>
2016-03-06 03:39:53 +0530

    landing page initials

---
+++ landing.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<!--[if IE 8]><html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]>
+<!--><html class="no-js" lang="en"><!--<![endif]-->
+
+<head>
+	<meta charset="utf-8" />
+    <title>Digital Fortress : Hack your heart out!</title>
+    <meta name="description" content="Digital Fortress : Hack your heart out!">
+    
+    <!-- Set the viewport width to device width for mobile -->
+    <meta name="viewport" content="width=device-width" />
+    <meta name="HandheldFriendly" content="True" />
+    <meta name="MobileOptimized" content="320" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="img/favicon/apple-touch-icon-144x144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/favicon/apple-touch-icon-114x114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/favicon/apple-touch-icon-72x72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="img/favicon/apple-touch-icon-57x57-precomposed.png">
+    <link rel="shortcut icon" href="img/favicon/favicon.png" />
+    
+    <!-- Included CSS Files -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style3.css">
+   
+    <!-- Google Web Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Exo:100,200,300,400,600,700,800,900,200italic,300italic,400italic' rel='stylesheet' type='text/css'>
+
+    <link rel="stylesheet" href="styleswitcher/css/styleswitcher.css" /> 
+</head>
+<body>
+	
+	<div id="ut_styleswitcher">
+		<div style="position: relative;">
+            <a href="#" class="toggle"></a>
+            <h1>Control Panel</h1>
+            <p class="info">Hi xyz! </p>
+            <p>
+     		<a class="button-switcher" href="#">Terminal</a>
+     		<a class="button-switcher" href="#">Leaderboard</a>
+     		</p>
+            
+        </div>
+    </div>
+	
+	<div id="loading">
+    	<img src="img/loader.gif" alt="Website Loader"/>
+    </div>
+	<div id="bgimg"></div>
+    <div class="slideshow-pattern"></div>
+    
+    <div class="container">
+    <div class="row">
+    <div class="span8 offset2" id="home">
+    
+    	<div id="part1" class="fadeout_1">
+	        <a class="a-rounded" href="#" title=""><h2>Hack your ❤ out</h2></a>
+	        <h3>IEEE NSIT presents to you, Digital Fortress - The Online Hacking Event!</h3>
+	        <p class="timer">
+	          	<span class="days"></span><span class="daysText"></span><span class="separate">/</span> 
+	          	<span class="hours"></span><span class="hoursText"></span><span class="separate">/</span>
+	          	<span class="minutes"></span><span class="minutesText"></span><span class="separate">/</span>
+	          	<span class="seconds"></span><span class="secondsText"></span> 
+			</p>
+		</div>	
+		
+		<div id="part2" class="fadeout_2">
+			<div class="row">
+			<div class="span8 links">
+				<div class="row">
+				<!-- @prerna: Login form will go here, also remove the below subscriber's form -->
+				<div id="part2_1" class="fadeout_2_1">
+					<div class="span2 offset2">
+						<a href="#myModal" id="meetus" data-toggle="modal">MEET US</a>
+					</div>
+				</div>
+				
+				<div id="part2_2" class="fadeout_2_2">		
+					<div class="span2">
+						<a id="link-map" href="#map">CONTACT US</a>
+					</div>
+				</div>
+				
+				</div>
+			</div>
+			</div> 
+		</div>
+			
+		<div id="part3" class="fadeout_3">
+			<div class="singup" >
+				<div class="subscribe-mailchimp">
+                    <p>Sign up now to our newsletter and you'll be one of the first to know the new site:</p>
+					<div id="mc_embed_signup">
+						<form id="mc-form">
+					        <input id="mc-email" type="email" placeholder="email" class="email">
+					        <button type="submit" class="btn"><span class="singup-image" ><img src="img/send.png" alt="send email" /></span><span class="singup-text">Send</span></button>
+					        <label for="mc-email"></label>
+					    </form>
+					</div>
+                </div>
+			</div>
+		</div>
+		
+	    </div>
+    </div>
+    </div>
+    
+    <div class="map-content">
+    
+    <div class="slideshow-pattern-map"></div>
+    
+    <div id="map"></div>
+	
+	<div class="container">
+	<div class="row">
+		<div class="span6 offset3 contact">	
+		
+		<div id="part4">
+            <h2>CONTACT US</h2>
+            <h4>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</h4>
+        </div>
+            <form action="#" id="contactform">
+                <p id="part5_1">
+                	<input type="text" name="name" class="input" placeholder="Name">
+				</p>
+				<p id="part5_2">
+					<input type="text" name="email" class="input" placeholder="Email">
+				</p>
+				<p id="part5_3">
+					<textarea name="message" cols="35" rows="5" class="textarea" placeholder="Message"></textarea>
+				</p>
+				<p id="part5_4">
+					<button class="button" type="submit" value="Send"><span class="singup-image"><img src="img/send.png" alt="send email" /></span><span class="singup-text">Send</span></button>
+				</p>
+			</form>
+			<div class="success-message-2"></div>
+            <div class="error-message-2"></div> 
+		</div>            	
+	</div> 
+	</div>       
+    
+    
+    <div class="footer">
+    <div id="part6">
+	<div class="container">
+		<div class="span12 back-to-top">
+			<a href="#home" id="home-top"><img class="back-to-top-1" src="img/back-to-top.png" alt="Go back to top" /><img class="back-to-top-2" src="img/back-to-top-hover.png" alt="Go back to top" /></a>
+		</div>
+		<div class="row">
+			<div class="span12">
+			<p class="footer-social">				
+				<a href="#" title="Facebook" data-gal="tooltip" data-placement="top" data-original-title="Facebook"><span class='symbol'>&#xe227;</span></a>
+				<a href="#" title="Twitter" data-gal="tooltip" data-placement="top" data-original-title="Twitter"><span class='symbol'>&#xe286;</span></a>
+				<a href="#" title="Pinterest" data-gal="tooltip" data-placement="top" data-original-title="Pinterest"><span class='symbol'>&#xe264;</span></a>
+				<a href="#" title="Skype" data-gal="tooltip" data-placement="top" data-original-title="Skype"><span class='symbol'>&#xe274;</span></a>
+				<a href="#" title="Flickr" data-gal="tooltip" data-placement="top" data-original-title="Flickr"><span class='symbol'>&#xe229;</span></a>
+				<a href="#" title="Google Plus" data-gal="tooltip" data-placement="top" data-original-title="Google Plus"><span class='symbol'>&#xe239;</span></a>
+				<a href="#" title="Picassa" data-gal="tooltip" data-placement="top" data-original-title="Picassa"><span class='symbol'>&#xe263;</span></a>
+				<a href="#" title="Linkedin" data-gal="tooltip" data-placement="top" data-original-title="Linkedin"><span class='symbol'>&#xe252;</span></a>
+				<a href="#" title="Youtube" data-gal="tooltip" data-placement="top" data-original-title="Youtube"><span class='symbol'>&#xe299;</span></a>
+				<a href="#" title="Instagram" data-gal="tooltip" data-placement="top" data-original-title="Instagram"><span class='symbol'>&#xe300;</span></a>
+			</p>
+			</div>
+		</div>
+	</div>
+	</div>
+	</div>
+	
+    </div> <!-- /map-content -->
+ 	
+ 	<a id="bgndVideo"  data-property="{videoURL:'https://www.youtube.com/watch?v=TeV9VTCRoS0',containment:'body',autoPlay:true, mute:false, startAt:0,opacity:1,ratio:'4/3', addRaster:true}">My video</a>
+ 	 
+	<!-- Javascript -->
+    <script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/device.min.js"></script>
+	<script type="text/javascript" src="js/jquery.countdown.js"></script>
+	<script type="text/javascript" src="js/jquery.ajaxchimp.js"></script>
+	<script type="text/javascript" src="js/ajaxchimp.js"></script>
+	<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+    <script type="text/javascript" src="js/jquery.gmap.min.js"></script>
+    <script type="text/javascript" src="js/jquery.scrollTo-1.4.3.1-min.js"></script>
+    <script type="text/javascript" src="js/scripts.js"></script>
+    <script type="text/javascript" src="js/jquery.mb.YTPlayer.js"></script>
+    <script type="text/javascript" src="js/video_youtube.js"></script>
+  	
+  	<!-- DEMO Switcher
+  	================================================== -->
+  	<script src="styleswitcher/js/styleswitcher.js"></script>
+  	
+</body>
+</html>
