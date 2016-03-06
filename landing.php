58c4d12a8cf8a993b3ab3599cf43825c99b633c3
prerna kakkar <pkakkar95@gmail.com>
2016-03-06 19:38:05 +0530

    google analytics

--- landing.php
+++ landing.php
@@ -114,6 +114,16 @@ session_start();
     });
   }
 </script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-74754564-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 
 	<div id="ut_styleswitcher">
 		<div style="position: relative;">
